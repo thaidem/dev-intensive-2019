a77d5eb591b00a166d182a30fabe4f396d1222fc
Dmitry Kozlov <thaidem19@gmail.com>
2020-01-23 15:38:30 +0300

    Реализовали Паттерн Builder

--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -326,4 +326,42 @@ class ExampleUnitTest {
         assertEquals("311 дней", TimeUnits.DAY.plural(311))
         assertEquals("1234 дня", TimeUnits.DAY.plural(1234))
     }
+
+    @Test
+    fun builderTest(){
+        val date = Date()
+
+        val user1 = User(
+            "5",
+            "Никола",
+            "Тесла",
+            null,
+            0,
+            1000,
+            date.add(-2, TimeUnits.DAY),
+            false)
+
+        val user2 = User.Builder().id("5")
+            .firstName("Никола")
+            .lastName("Тесла")
+            .avatar(null)
+            .rating(0)
+            .respect(1000)
+            .lastVisit(date.add(-2, TimeUnits.DAY))
+            .isOnline(false)
+            .build()
+
+        val user3 = User.Builder().build()
+
+        assertEquals(user1, user2)
+        assertTrue(user3 is User)
+        assertNotEquals(null, user3.id)
+        assertEquals(null, user3.firstName)
+        assertEquals(null, user3.lastName)
+        assertEquals(null, user3.avatar)
+        assertEquals(0, user3.rating)
+        assertEquals(0, user3.respect)
+        assertNotEquals(null, user3.lastVisit)
+        assertFalse(user3.isOnline)
+    }
 }
